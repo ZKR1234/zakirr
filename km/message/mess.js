c38b3a038a20e700b093c27e50e8ffee9ddf94fc
Zakir <fikisosad999@gmail.com>
2022-02-16 01:26:13 +0700

    zakirr

---
+++ km/message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'DONE BRO...',
+			wrongFormat: 'GADA BRO...',
+			error: {
+				api: 'APIKEY EXPIRED BRO...',
+				stick: 'NOT STICKER BRO...',
+				Iv: 'GOSAH SEND LINK BRO...'
+			},
+			only: {
+				group: 'ONLY GROUP CHAT BRO...',
+				admin: 'ONLY ADMIN GROUP BRO...',
+				premium: 'ONLY USER PREMIUM BRO...',
+				owner: 'ONLY OWNER BRO...',
+				Badmin: 'ADMININ BOT DULU BRO...',
+			}
+		}
